0f300c95059b851445abec5367fb215adb7a3871
Khondwani-Banda <banda.khondwani@aims-senegal.org>
2025-05-26 03:22:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -89,6 +89,6 @@ def tumor_info():
     return render_template("tumor_info.html")
 
 # IMPORTANT: for deployment
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
